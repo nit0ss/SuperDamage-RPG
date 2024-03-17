d1602d552834aa120a91a9899217db1898430973
nit0ss <nitoss@mail.com>
2024-03-17 03:20:58 +0100

    --
    Battle state player detection implementation

--- SuperDamage-RPG/Assets/Scripts/Enemy/Enemy.cs
+++ SuperDamage-RPG/Assets/Scripts/Enemy/Enemy.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Enemy : Entity
 {
+   [SerializeField]protected LayerMask whatIsPlayer;
    [Header("Move Info")]
    public float moveSpeed;
    public float idleTime;
+
 
    public EnemyStateMachine stateMachine { get; private set; }
 
@@ -20,8 +22,12 @@ public class Enemy : Entity
    {
       base.Update();
       stateMachine.currentState.Update();
+
+      Debug.Log(IsPlayerDetected().collider.gameObject.name + "I SEE");
       
 
    }
 
+
+   public virtual RaycastHit2D IsPlayerDetected() => Physics2D.Raycast(wallCheck.position, Vector2.right * facingDir, 50, whatIsPlayer);
 }
